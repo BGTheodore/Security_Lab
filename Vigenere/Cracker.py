9f406b8105e568489e5eb1e884679550c735c33c
JBAltidor <altidorjb@gmail.com>
2020-11-25 12:42:31 -0500

    Decoder test

--- Vigenere/Cracker.py
+++ Vigenere/Cracker.py
@@ -1,7 +1,17 @@
 from language_detector import detect_language
+from textwrap import wrap
+import re
 
-text = "Je suis en train de tester"
-text =   ''.join(text.split())
-language = detect_language(text)
-print (language)
-print (ord(""))
+# Driver code 
+if __name__ == "__main__": 
+        
+        string = "SOQSSYFDFDMJORCQ"
+        for size in range (1,6):
+            splited = wrap(string,size)
+        #ciphertext_to_int= [ord(i) for i in string]
+        #splited = wrap(ciphertext_to_int,2)
+        #re.findall('..?', ciphertext_to_int)
+        print(splited)
+                
+
+    
